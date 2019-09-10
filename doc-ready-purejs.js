ecf938c310578b7328ea8ecb600cbed891542315
Florence Davis <fpdavis86@gmail.com>
2019-09-10 10:57:27 -0400

    Doc Ready Pure JS
    Doc Ready Pure JS

---
+++ doc-ready-purejs.js
@@ -0,0 +1,20 @@
+function docReady(fn) {
+  // see if DOM is already available
+
+  if (
+    document.readyState === "complete" ||
+    document.readyState === "interactive"
+  ) {
+    // call on next available tick
+
+    setTimeout(fn, 1);
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
+}
+
+/* USAGE */
+
+docReady(function () {
+  // DOM is loaded and ready for manipulation here
+});
